143cb35914e1034b2f50339ee7d34b08c8504e6f
Nicolas Cornet <_ncornet@cremi.emi.u-bordeaux1.fr>
2024-01-27 17:30:19 +0100

    animation

--- JustSayYes/Assets/Scripts/Player.cs
+++ JustSayYes/Assets/Scripts/Player.cs
@@ -12,41 +12,30 @@ public class Player : MonoBehaviour
 
     CharacterController cc;
     Vector2 target;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         target = new Vector2(transform.position.x, transform.position.y);
         cc = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (canMove)
         {
             MouseInput();
             Movement();
         }
-<<<<<<< HEAD
     }
 
     void MouseInput(){
         if(Input.GetButton("Fire1")){
             if(Physics2D.OverlapCircle(Camera.main.ScreenToWorldPoint(Input.mousePosition), 0.01f,walkableLayer) != null){
-=======
-
-
-
-    }
-
-    void MouseInput()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            if (Physics2D.OverlapCircle(Camera.main.ScreenToWorldPoint(Input.mousePosition), 0.01f, walkableLayer) != null)
-            {
->>>>>>> 005e312080727930eee9abc521fe765ebfd12561
                 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             }
             else
@@ -56,26 +45,21 @@ public class Player : MonoBehaviour
         }
     }
 
-<<<<<<< HEAD
     void Movement(){
         Vector2 dir = (target-new Vector2(transform.position.x,transform.position.y));
+        if(dir.x > 0){
+            transform.localScale = new Vector3(-1,1,1);
+        }else{
+            transform.localScale = new Vector3(1,1,1);
+        }
         if(dir.magnitude < 0.1){
-=======
-    void Movement()
-    {
-        Vector2 dir = (target - new Vector2(transform.position.x, transform.position.y));
-
-        if (dir.magnitude < 0.1)
-        {
->>>>>>> 005e312080727930eee9abc521fe765ebfd12561
             dir = Vector2.zero;
             target = new Vector2(transform.position.x, transform.position.y);
+            anim.SetBool("isWalking",false);
+        }else{
+            anim.SetBool("isWalking",true);
         }
         dir.Normalize();
-<<<<<<< HEAD
         cc.Move(dir*walkSpeed*Time.deltaTime);
-=======
-        cc.Move(dir * walkSpeed * Time.deltaTime);
->>>>>>> 005e312080727930eee9abc521fe765ebfd12561
     }
 }
