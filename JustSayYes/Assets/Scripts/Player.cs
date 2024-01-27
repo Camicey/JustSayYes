62f04c98d260a66361cd1ad1bea259d18d7deb19
Camice <cgazi@ensc.fr>
2024-01-27 16:00:06 +0100

    Images Nono, tremblement de terre

--- JustSayYes/Assets/Scripts/Player.cs
+++ JustSayYes/Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+
     public float walkSpeed;
     public LayerMask walkableLayer;
     public bool canMove = true;
@@ -15,42 +15,48 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        target = new Vector2(transform.position.x,transform.position.y);
+        target = new Vector2(transform.position.x, transform.position.y);
         cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(canMove){
+        if (canMove)
+        {
             MouseInput();
             Movement();
         }
 
-        
-       
+
+
     }
 
-    void MouseInput(){
-        if(Input.GetButtonDown("Fire1")){
-            if(Physics2D.OverlapCircle(Camera.main.ScreenToWorldPoint(Input.mousePosition), 0.01f,walkableLayer) != null){
+    void MouseInput()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (Physics2D.OverlapCircle(Camera.main.ScreenToWorldPoint(Input.mousePosition), 0.01f, walkableLayer) != null)
+            {
                 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             }
             else
             {
-                target = new Vector2(transform.position.x,transform.position.y);
+                target = new Vector2(transform.position.x, transform.position.y);
             }
         }
     }
 
-    void Movement(){
-        Vector2 dir = (target-new Vector2(transform.position.x,transform.position.y));
-        dir.Normalize();
-        if(dir.magnitude < 0.1){
-            dir = Vector2.zero;
-            target = new Vector2(transform.position.x,transform.position.y);
-        }
+    void Movement()
+    {
+        Vector2 dir = (target - new Vector2(transform.position.x, transform.position.y));
 
-        cc.Move(dir*walkSpeed*Time.deltaTime);
+        if (dir.magnitude < 0.1)
+        {
+            dir = Vector2.zero;
+            target = new Vector2(transform.position.x, transform.position.y);
+        }
+        dir.Normalize();
+        cc.Move(dir * walkSpeed * Time.deltaTime);
     }
 }
