ac662ab12e575834185ee21c971f2b57df118f00
Camice <cgazi@ensc.fr>
2024-01-28 19:14:16 +0100

    petite erreur

--- JustSayYes/Assets/Scripts/ClickToSkip.cs
+++ JustSayYes/Assets/Scripts/ClickToSkip.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ClickToSkip : MonoBehaviour
 {
@@ -8,13 +9,13 @@ public class ClickToSkip : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-         if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1"))
         {
             SceneManager.LoadScene(nextScene);
         }
